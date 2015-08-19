1bd37ee743b8b8cba2a8173f81a4130cce4621ea
Jesse Wolfe <jes5199@gmail.com>
2015-08-19 13:38:15 -0700

    autogrow and button

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -2,7 +2,6 @@ class NodeController < ApplicationController
   def view
       @name = params[:name]
       @nodes = Node.where(name: @name)
-      @nodes = [Node.new(name: @name, body: "hello I am node", author: User.new(name: "jes"), created_at: Time.now)]
   end
 
   def zoom
