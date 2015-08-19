bb30b1d719d1ad38c2b50b31c6fd35c5bb6e606c
Jesse Wolfe <jes5199@gmail.com>
2015-08-19 13:57:52 -0700

    more edity ui

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -2,6 +2,7 @@ class NodeController < ApplicationController
   def view
       @name = params[:name]
       @nodes = Node.where(name: @name)
+      @your_node = @nodes.where(author: current_user).first()
   end
 
   def zoom
