c5768d5eb2fc4c5ab3240877bad564a81cc6afc7
Jesse Wolfe <jes5199@gmail.com>
2015-10-04 02:43:16 -0700

    login works

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,13 @@
 class UserController < ApplicationController
     def login
+        if request.post?
+            user = User.find_by_name(params[:name]).try(:authenticate, params[:pass])
+            if user
+                session[:user] = user
+                redirect_to("/*/welcome")
+            else
+                flash[:error] = "ACCESS DENIED"
+            end
+        end
     end
 end
