364358848d130b6dc734aedfde5aeb51087ce0eb
Jesse Wolfe <jes5199@gmail.com>
2015-10-04 04:25:39 -0700

    respect namespaces

--- app/controllers/node_controller.rb
+++ app/controllers/node_controller.rb
@@ -3,13 +3,13 @@ class NodeController < ApplicationController
     if request.env["REQUEST_URI"] != request.url
       return redirect_to(request.url)
     end
+    @namespace = params[:namespace]
     @name = params[:name]
-    @nodes = Node.where(name: @name)
-    @your_node = @nodes.where(author: current_user).first() || Node.new(name: @name, author: current_user)
+    @nodes = Node.where(name: @name, namespace: @namespace)
+    @your_node = @nodes.where(author: current_user).first() || Node.new(name: @name, author: current_user, namespace: @namespace)
     if request.post?
       @your_node.body = params[:node]
       @your_node.save!
-      @nodes = Node.where(name: @name)
     end
   end
 
