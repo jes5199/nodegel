70d7dcd8839bfd9b1268528fbbf399637cf3ef26
Jesse Wolfe <jes5199@gmail.com>
2015-10-07 20:10:12 -0700

    strict validation on user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates_format_of :name, with: /\A[a-zA-Z0-9 _-]+\z/
+  validate :no_weird_spacing
 
   def to_s
     return self.name
+  end
+
+  def no_weird_spacing
+    if name != name.strip.gsub("  ", " ")
+      errors.add(:name, "can't have weird spaces")
+    end
   end
 end
